15119c61cc57753c323117e794ddb5e69fcd5bee
dedobbin <you@example.com>
2023-02-02 20:35:01 +0100

    simplified gluing logic

--- video_glue.py
+++ video_glue.py
@@ -4,11 +4,9 @@ import ffmpeg
 from pathlib import Path
 import shutil
 import os
+from datetime import datetime
+import glob
 
-
-def should_stop():
-    return False
-    #return randrange(0,20)==0
 
 def create_dir(path):
     if os.path.exists(path) and not os.path.isdir(path):
@@ -19,56 +17,59 @@ def create_dir(path):
         os.mkdir(path)
     return True
 
-def stitch(should_stop, incoming_dir="incoming/", output_dir="output/", processed_dir="processed/"):
-    create_dir(incoming_dir)
-    create_dir(output_dir)
-    create_dir(processed_dir)
-    
-    output_file = f"output_{time.time()}.mp4" #name of final path
-    stitch_file_path = "stitch_list.txt"
-    
-    with open(stitch_file_path, 'w') as f:
-        f.write(f"file '{output_dir+output_file}'\n")
-    
-    while(True):
+def create_stitch_list_file(path):
+    with open(path,'w') as f:
+        print(f"Created empty stitch list at {os.path.abspath(path)}")
+
+def build_stitch_list(should_stop, incoming_dir):
+    stitch_list = []
+    print("Build stitch list")
+    while True:
         if should_stop():
-            print("Stopping")
-            return output_dir + output_file
-            
-        #print(f"Continue stitching, checking {incoming_dir}")
-        for incoming_file in os.listdir(incoming_dir):
-            if incoming_file not in os.listdir(processed_dir):
-                print(f"Found unprocessed file {incoming_file}")
-                if not os.path.exists(output_dir+output_file):
-                    print(f"{incoming_file} is first, copy to output directly")
-                    shutil.copy(incoming_dir+incoming_file, output_dir+output_file)
-                else:
-
-                    with open(stitch_file_path, "w") as f:
-                        f.write(f"file '{output_dir+output_file}'\n")
-                        f.write(f"file '{incoming_dir+incoming_file}'\n")
-
-                    
-                    with open(stitch_file_path, 'r') as f:
-                        print(f"stitching:\n{f.read()}")
-                    ffmpeg.input(stitch_file_path, f='concat', safe=0).output(output_dir+"tmp.mp4", codec='copy', loglevel="quiet").overwrite_output().run()
-                    Path(output_dir+"tmp.mp4").rename(output_dir+output_file) #move
-
-                # print(f"moving {incoming_file} to {processed_dir}")
-                # Path(incoming_dir+incoming_file).rename(processed_dir+incoming_file) #move
-                os.remove(incoming_dir+incoming_file) 
-
-            else:
-                print(f"{incoming_file} is already processed")
+            print("Got stop signal, will return stitch list")
+            return stitch_list
         
+        #print(f"Files in incoming dir: ", len(os.listdir(incoming_dir)))
+        for incoming_file in os.listdir(incoming_dir):
+            file_path = incoming_dir+incoming_file           
+            if file_path in stitch_list:
+                continue
+            print("Inserted in stitch list ", file_path)
+            stitch_list.append(file_path)
         time.sleep(5)
 
-def main():
-    while True:
-        print("Start stitching")
-        stitch(should_stop)
-        print("End stitching")
+def stitch(should_stop, incoming_dir="incoming/", output_dir="output/"):
+    resulting_filepath = f"{output_dir}output_{datetime.timestamp(datetime.now())}.mp4"
+
+    create_dir(incoming_dir)
+    create_dir(output_dir)
+
+    stitch_file_path = "stitch_list.txt"
+    create_stitch_list_file(stitch_file_path)
+
+
+    print("Start building stitch list")
+    stitch_list = build_stitch_list(should_stop, incoming_dir)
+    
+    print("Got end signal, will glue")
+    
+    if len(stitch_list) > 0:
+        with open(stitch_file_path, 'w') as f:
+            for file_path in stitch_list:
+                f.write(f"file '{file_path}'\n")
+        ffmpeg.input(stitch_file_path, f='concat', safe=0).output(resulting_filepath, codec='copy', loglevel='quiet').overwrite_output().run()
+    if not os.path.exists(resulting_filepath) or not os.path.isfile(resulting_filepath):
+        return False
+
+    # cleanup to prepare for next round
+    create_stitch_list_file(stitch_file_path)
+    files = glob.glob(incoming_dir + "*")
+    for f in files:
+        os.remove(f)
+
+    print ("Successfully created " + resulting_filepath)
+    return resulting_filepath
+
 
 if __name__ == "__main__":
-    main()
-
+    stitch(lambda : False, "incoming/", "output/")
